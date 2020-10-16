1506129889fc0e7c3d4c04c4f3dd5481cb96e440
johne5s <johne5@yahoo.com>
2020-10-15 22:17:43 -0400

    Separated CSS files
    New Branch started for Version 2 of the Inventory system.
    This is the fist update with removing some unused files and moved around a few files.
    Pulled out CSS and created there own CSS files

---
+++ Source/WEB_ROOT/admin/asset/js/addHistory.js
@@ -0,0 +1,22 @@
+function addRecordToHistoryDB(assetid,note)
+    {
+        //console.log("Add new - " + assetid);
+        $psq('u_assethistory').insert(
+            {
+                Asset_ID: assetid,
+                History_DateTime: curDate(),
+                Notes: note
+            });
+
+    }
+
+function curDate()
+{
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = ("0" + (d.getMonth()+1)).slice(-2);
+    var day = ("0" + d.getDate()).slice(-2);
+    var currentDate = year + "/" + month + "/" + day + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
+
+    return currentDate;
+}
